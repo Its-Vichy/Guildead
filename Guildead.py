3600f75fdec2d5b9e60b64f12b042f0aa8ce1365
0хVιcнy#1337 <Its_Vichy@protonmail.com>
2022-01-22 01:47:17 +0100

    Using requests and not httpx session

--- Guildead.py
+++ Guildead.py
@@ -1,4 +1,4 @@
-import httpx, uuid, random, string
+import requests, uuid, random, string
 
 class Exploit:
     def blank_message():
@@ -7,15 +7,12 @@ class Exploit:
 class Guilded:
     def __init__(self, proxy: str= None):
         self.base_url = "https://www.guilded.gg/api"
-        self.session = httpx.Client(proxies= {"http://": proxy, "https://": proxy} if proxy else None)
+        self.session = requests.Session()
+        self.session.proxies = {"http": proxy, "https": proxy} if proxy else None
 
     def login(self, email: str, password: str):
         r = self.session.post(f'{self.base_url}/login', json={'email': email, 'password': password})
-
-        if 'Email or password is incorrect.' in r.text:
-            return False
-        else:
-            return (r.cookies.get('guilded_mid'), r.cookies.get('hmac_signed_session'))
+        return (False, {'error': 'Email or password is incorrect.'}) if 'Email or password is incorrect.' in r.text in r.text else (True, {'mid': r.cookies.get('guilded_mid'), 'hmac_signed_session': r.cookies.get('hmac_signed_session')})
     
     def send_message(self, channel_id: str, message: str, confirmed: bool= False, isSilent: bool= False, isPrivate: bool= False, repliesTo: list= []):
         r = self.session.post(f'{self.base_url}/channels/{channel_id}/messages', json={
